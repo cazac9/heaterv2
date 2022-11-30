e4052248392a186e89513ae23fc5cb5a1c0631f2
ohumeniuk <ohumeniuk@crownresorts.com.au>
2022-11-30 12:03:41 +0200

    first commit

---
+++ main/main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+
+void app_main(void)
+{
+    printf("Hello, Tester!\n");
+    printf("Simple calculating...");
+    printf("Ok. \nLet's calculate an infinite sequence!\n");
+    printf("You can't get here - the sequence is INFINITE!\n");
+}
